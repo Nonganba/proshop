4f2d8a95de2d25faad5dedf085cb7d500b9a54d2
Nonganba <nanganba.th1109@gmail.com>
2024-05-20 14:48:24 +0530

    implement logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,7 +20,7 @@ const authUser = asyncHandler(async (req, res) => {
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
-    
+
     res.json({
       _id: user._id,
       name: user.name,
@@ -44,7 +44,14 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
 });
 
 // @desc    Get user profile
